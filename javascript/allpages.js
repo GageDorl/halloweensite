b9a6bd1d8b718c224cca0bb199a4c84aab6b9685
GageDorl <gagedorlando@gmail.com>
2024-10-15 00:29:05 -0600

    previous working version

--- javascript/allpages.js
+++
@@ -1,8 +0,0 @@
-var range = 1 / 1000;
-var vw = range * Math.min(window.innerWidth, window.innerHeight);
-
-document.documentElement.style.setProperty('--vw-scale', `${vw}`);
-
-window.addEventListener('resize', () => {
-  document.documentElement.style.setProperty('--vw-scale', `${range * Math.min(window.innerWidth, window.innerHeight)}`);
-});
